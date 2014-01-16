ac106e6ff6ec030f257d2b4d3051be8dc696b74c
Tatsuo Ikeda <jp.ne.co.jp@gmail.com>
2014-01-17 04:23:15 +0900

    initial

---
+++ bookmarkhub.coffee
@@ -0,0 +1,352 @@
+window.Bookmarkhub or= {}
+BH = window.Bookmarkhub
+
+BH.expires = 600000
+
+BH.trace = (rest...) ->
+  console.log rest if no
+
+BH.storage = {
+  save: (key, value, expires=0) ->
+    return no unless localStorage
+
+    record =
+      value: JSON.stringify(value)
+      timestamp: new Date().getTime() + expires
+
+    # BH.trace key, JSON.stringify(record), expires
+    localStorage.setItem(key, JSON.stringify(record))
+
+    value
+  ,
+  load: (key) ->
+    return no unless localStorage
+
+    record = JSON.parse(localStorage.getItem(key))
+
+    if record
+      new Date().getTime() < record.timestamp and JSON.parse(record.value)
+    else
+      no
+}
+
+
+class BH.URLs
+
+  @twitter: (url) ->
+    "http://urls.api.twitter.com/1/urls/count.json?url=#{encodeURIComponent url}"
+
+  @facebook: (url) ->
+    "https://graph.facebook.com/?id=#{encodeURIComponent url}"
+
+  @hatena: (url) ->
+    "http://api.b.st-hatena.com/entry.count?url=#{encodeURIComponent url}"
+
+  @google: (url) ->
+    apiUrl = "https://plus.google.com/_/+1/fastbutton?hl=ja&url=#{url}"
+    yql = encodeURIComponent("SELECT content FROM data.headers WHERE url='#{apiUrl}'")
+    "http://query.yahooapis.com/v1/public/yql?q=#{yql}&format=json&env=http://datatables.org/alltables.env&diagnostics=false"
+
+  # TODO: YQL(pocket) query bugfux: cleaned url fragment(#).
+  @pocket: (url) ->
+    apiUrl = "https://widgets.getpocket.com/v1/button?label=pocket&count=vertical&align=left&v=1&title=&url=#{url}&src=#{url}&r=#{Math.floor(Math.random() * 10000000)}"
+    yql = encodeURIComponent("SELECT * FROM html WHERE url='#{apiUrl}'")
+    "http://query.yahooapis.com/v1/public/yql?q=#{yql}&format=json&diagnostics=false"
+
+  @linkedin: (url) ->
+    "http://www.linkedin.com/countserv/count/share?url=#{encodeURIComponent url}"
+
+  @delicious: (url) ->
+    "http://feeds.delicious.com/v2/json/urlinfo/data?url=#{encodeURIComponent url}"
+
+  @pinterest: (url) ->
+    "http://api.pinterest.com/v1/urls/count.json?url=#{encodeURIComponent url}"
+
+  @stumbleupon: (url) ->
+    apiUrl = "http://www.stumbleupon.com/services/1.01/badge.getinfo?url=#{encodeURIComponent url}"
+    yql = "SELECT * FROM json WHERE url='#{apiUrl}'"
+    "http://query.yahooapis.com/v1/public/yql?q=#{yql}&format=json&diagnostics=true&callback="
+
+
+class BH.Counter
+
+  constructor: (@url, @expires=BH.expires) ->
+
+  cacheKey: (key) ->
+    "Bookmarkhub.Counter.cacheKey(#{key})"
+
+  cachedRequest: (url, rest...) ->
+    callback = rest.pop()
+    options = rest.pop() || {}
+
+    result = BH.storage.load @cacheKey(url)
+    if result
+      BH.trace "cached: #{url}"
+      return callback result
+
+    me = @
+    @request url, options, (data) ->
+      BH.storage.save me.cacheKey(url), data, me.expires
+      callback data
+
+  request: (url, rest...) ->
+    callback = rest.pop()
+    options = $.extend
+        url: url
+        type: 'get'
+        dataType: 'jsonp'
+      ,
+        rest.pop() || {}
+
+    $.ajax(
+      options
+    ).done((data) ->
+      callback data
+    ).fail (rest...) ->
+      BH.trace 'ajax fail: ', rest
+
+  twitter: (callback) ->
+    @cachedRequest BH.URLs.twitter(@url), (data) ->
+      result = data.count or 0
+      callback result
+
+  hatena: (callback) ->
+    @cachedRequest BH.URLs.hatena(@url), (data) ->
+      callback data or 0
+
+  facebook: (callback) ->
+    @cachedRequest BH.URLs.facebook(@url), (data) ->
+      callback data.shares or 0
+
+  google: (callback) ->
+    @cachedRequest BH.URLs.google(@url), dataType: 'json', (data) ->
+      callback if data.query.results
+        $(data.query.results.resources.content)
+          .find('#aggregateCount').text()
+      else
+        0
+
+  pocket: (callback) ->
+    # debugger
+    result = BH.storage.load @cacheKey(@url)
+    if result
+      BH.trace "cached: #{@url}"
+      return callback result
+
+    me = @
+    @request BH.URLs.pocket(@url), dataType: 'json', (data) ->
+      result = if data.query
+        data.query.results
+          .body.div.a.span.em.content
+      else
+        0
+      BH.storage.save me.cacheKey(me.url), result, me.expires
+      callback result
+
+  linkedin: (callback) ->
+    @cachedRequest BH.URLs.linkedin(@url), (data) ->
+      callback data.count or 0
+
+  delicious: (callback) ->
+    @cachedRequest BH.URLs.delicious(@url), (data) ->
+      callback data[0]?.total_posts or 0
+
+  pinterest: (callback) ->
+    @cachedRequest BH.URLs.pinterest(@url), (data) ->
+      callback data.count or 0
+
+  stumbleupon: (callback) ->
+    @cachedRequest BH.URLs.stumbleupon(@url), dataType: 'json', (data) ->
+      callback data.query
+        .results.json.result?.views or 0
+
+
+class BH.Linker
+  constructor: (@url, @expires=BH.expires) ->
+    @counter = new BH.Counter(@url, @expires)
+
+  schemelessUrl: ->
+    encodeURIComponent(@url.replace(/^https?:\/\//, ''))
+
+  twitter: (callback) ->
+    me = @
+    @counter.twitter (count) ->
+      callback
+        count: count
+        link: "https://twitter.com/search?q=#{me.schemelessUrl()}" if count
+
+  facebook: (callback) ->
+    @counter.facebook (count) ->
+      callback
+        count: count
+
+  hatena: (callback) ->
+    me = @
+    @counter.hatena (count) ->
+      callback
+        count: count
+        link: "http://b.hatena.ne.jp/entry/#{me.schemelessUrl()}" if count
+
+  google: (callback) ->
+    @counter.google (count) ->
+      callback
+        count: count
+
+  pocket: (callback) ->
+    @counter.pocket (count) ->
+      callback
+        count: count
+
+  linkedin: (callback) ->
+    me = @
+    @counter.linkedin (count) ->
+      callback
+        count: count
+
+  delicious: (callback) ->
+    me = @
+    @counter.delicious (count) ->
+      callback
+        count: count
+        link: "https://previous.delicious.com/url/#{BH.md5Hex me.url}" if count
+
+  pinterest: (callback) ->
+    me = @
+    @counter.pinterest (count) ->
+      callback
+        count: count
+
+  stumbleupon: (callback) ->
+    me = @
+    @counter.stumbleupon (count) ->
+      callback
+        count: count
+        link: "http://www.stumbleupon.com/url/#{me.url}" if count
+
+# TODO:
+BH.md5Hex = (->
+  MD5_F = (x, y, z) ->
+    (x & y) | (~x & z)
+  MD5_G = (x, y, z) ->
+    (x & z) | (y & ~z)
+  MD5_H = (x, y, z) ->
+    x ^ y ^ z
+  MD5_I = (x, y, z) ->
+    y ^ (x | ~z)
+  MD5_pack = (n32) ->
+    CC(n32 & 0xff) + CC((n32 >>> 8) & 0xff) + CC((n32 >>> 16) & 0xff) + CC((n32 >>> 24) & 0xff)
+  MD5_unpack = (s4) ->
+    s4.charCodeAt(0) | (s4.charCodeAt(1) << 8) | (s4.charCodeAt(2) << 16) | (s4.charCodeAt(3) << 24)
+  MD5_number = (n) ->
+    n += 4294967296  while n < 0
+    n -= 4294967296  while n > 4294967295
+    n
+  MD5_apply_round = (x, s, f, abcd, r) ->
+    a = undefined
+    b = undefined
+    c = undefined
+    d = undefined
+    kk = undefined
+    ss = undefined
+    ii = undefined
+    t = undefined
+    u = undefined
+    a = abcd[0]
+    b = abcd[1]
+    c = abcd[2]
+    d = abcd[3]
+    kk = r[0]
+    ss = r[1]
+    ii = r[2]
+    u = f(s[b], s[c], s[d])
+    t = s[a] + u + x[kk] + MD5_T[ii]
+    t = MD5_number(t)
+    t = ((t << ss) | (t >>> (32 - ss)))
+    t += s[b]
+    s[a] = MD5_number(t)
+  MD5_hash = (data) ->
+    abcd = undefined
+    x = undefined
+    state = undefined
+    s = undefined
+    len = undefined
+    index = undefined
+    padLen = undefined
+    f = undefined
+    r = undefined
+    i = undefined
+    j = undefined
+    k = undefined
+    tmp = undefined
+    state = [0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476]
+    len = data.length
+    index = len & 0x3f
+    padLen = (if (index < 56) then (56 - index) else (120 - index))
+    if padLen > 0
+      data += ""
+      i = 0
+      while i < padLen - 1
+        data += "\u0000"
+        i++
+    data += MD5_pack(len * 8)
+    data += MD5_pack(0)
+    len += padLen + 8
+    abcd = [0, 1, 2, 3]
+    x = new Array(16)
+    s = new Array(4)
+    k = 0
+    while k < len
+      i = 0
+      j = k
+
+      while i < 16
+        x[i] = data.charCodeAt(j) | (data.charCodeAt(j + 1) << 8) | (data.charCodeAt(j + 2) << 16) | (data.charCodeAt(j + 3) << 24)
+        i++
+        j += 4
+      i = 0
+      while i < 4
+        s[i] = state[i]
+        i++
+      i = 0
+      while i < 4
+        f = MD5_round[i][0]
+        r = MD5_round[i][1]
+        j = 0
+        while j < 16
+          MD5_apply_round x, s, f, abcd, r[j]
+          tmp = abcd[0]
+          abcd[0] = abcd[3]
+          abcd[3] = abcd[2]
+          abcd[2] = abcd[1]
+          abcd[1] = tmp
+          j++
+        i++
+      i = 0
+      while i < 4
+        state[i] += s[i]
+        state[i] = MD5_number(state[i])
+        i++
+      k += 64
+    MD5_pack(state[0]) + MD5_pack(state[1]) + MD5_pack(state[2]) + MD5_pack(state[3])
+  MD5_hexhash = (data) ->
+    i = undefined
+    out = undefined
+    c = undefined
+    bit128 = MD5_hash(data)
+    out = ""
+    i = 0
+    while i < 16
+      c = bit128.charCodeAt(i)
+      out += "0123456789abcdef".charAt((c >> 4) & 0xf)
+      out += "0123456789abcdef".charAt(c & 0xf)
+      i++
+    out
+  MD5_T = [0x00000000, 0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee, 0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501, 0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be, 0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821, 0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa, 0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8, 0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed, 0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a, 0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c, 0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70, 0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05, 0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665, 0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039, 0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1, 0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1, 0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391]
+  MD5_round1 = [[0, 7, 1], [1, 12, 2], [2, 17, 3], [3, 22, 4], [4, 7, 5], [5, 12, 6], [6, 17, 7], [7, 22, 8], [8, 7, 9], [9, 12, 10], [10, 17, 11], [11, 22, 12], [12, 7, 13], [13, 12, 14], [14, 17, 15], [15, 22, 16]]
+  MD5_round2 = [[1, 5, 17], [6, 9, 18], [11, 14, 19], [0, 20, 20], [5, 5, 21], [10, 9, 22], [15, 14, 23], [4, 20, 24], [9, 5, 25], [14, 9, 26], [3, 14, 27], [8, 20, 28], [13, 5, 29], [2, 9, 30], [7, 14, 31], [12, 20, 32]]
+  MD5_round3 = [[5, 4, 33], [8, 11, 34], [11, 16, 35], [14, 23, 36], [1, 4, 37], [4, 11, 38], [7, 16, 39], [10, 23, 40], [13, 4, 41], [0, 11, 42], [3, 16, 43], [6, 23, 44], [9, 4, 45], [12, 11, 46], [15, 16, 47], [2, 23, 48]]
+  MD5_round4 = [[0, 6, 49], [7, 10, 50], [14, 15, 51], [5, 21, 52], [12, 6, 53], [3, 10, 54], [10, 15, 55], [1, 21, 56], [8, 6, 57], [15, 10, 58], [6, 15, 59], [13, 21, 60], [4, 6, 61], [11, 10, 62], [2, 15, 63], [9, 21, 64]]
+  MD5_round = [[MD5_F, MD5_round1], [MD5_G, MD5_round2], [MD5_H, MD5_round3], [MD5_I, MD5_round4]]
+  CC = String.fromCharCode
+  MD5_hexhash
+)()
---
+++ bookmarkhub.js
@@ -0,0 +1,479 @@
+(function() {
+  var BH,
+    __slice = [].slice;
+
+  window.Bookmarkhub || (window.Bookmarkhub = {});
+
+  BH = window.Bookmarkhub;
+
+  BH.expires = 600000;
+
+  BH.trace = function() {
+    var rest;
+    rest = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
+    if (false) {
+      return console.log(rest);
+    }
+  };
+
+  BH.storage = {
+    save: function(key, value, expires) {
+      var record;
+      if (expires == null) {
+        expires = 0;
+      }
+      if (!localStorage) {
+        return false;
+      }
+      record = {
+        value: JSON.stringify(value),
+        timestamp: new Date().getTime() + expires
+      };
+      localStorage.setItem(key, JSON.stringify(record));
+      return value;
+    },
+    load: function(key) {
+      var record;
+      if (!localStorage) {
+        return false;
+      }
+      record = JSON.parse(localStorage.getItem(key));
+      if (record) {
+        return new Date().getTime() < record.timestamp && JSON.parse(record.value);
+      } else {
+        return false;
+      }
+    }
+  };
+
+  BH.URLs = (function() {
+    function URLs() {}
+
+    URLs.twitter = function(url) {
+      return "http://urls.api.twitter.com/1/urls/count.json?url=" + (encodeURIComponent(url));
+    };
+
+    URLs.facebook = function(url) {
+      return "https://graph.facebook.com/?id=" + (encodeURIComponent(url));
+    };
+
+    URLs.hatena = function(url) {
+      return "http://api.b.st-hatena.com/entry.count?url=" + (encodeURIComponent(url));
+    };
+
+    URLs.google = function(url) {
+      var apiUrl, yql;
+      apiUrl = "https://plus.google.com/_/+1/fastbutton?hl=ja&url=" + url;
+      yql = encodeURIComponent("SELECT content FROM data.headers WHERE url='" + apiUrl + "'");
+      return "http://query.yahooapis.com/v1/public/yql?q=" + yql + "&format=json&env=http://datatables.org/alltables.env&diagnostics=false";
+    };
+
+    URLs.pocket = function(url) {
+      var apiUrl, yql;
+      apiUrl = "https://widgets.getpocket.com/v1/button?label=pocket&count=vertical&align=left&v=1&title=&url=" + url + "&src=" + url + "&r=" + (Math.floor(Math.random() * 10000000));
+      yql = encodeURIComponent("SELECT * FROM html WHERE url='" + apiUrl + "'");
+      return "http://query.yahooapis.com/v1/public/yql?q=" + yql + "&format=json&diagnostics=false";
+    };
+
+    URLs.linkedin = function(url) {
+      return "http://www.linkedin.com/countserv/count/share?url=" + (encodeURIComponent(url));
+    };
+
+    URLs.delicious = function(url) {
+      return "http://feeds.delicious.com/v2/json/urlinfo/data?url=" + (encodeURIComponent(url));
+    };
+
+    URLs.pinterest = function(url) {
+      return "http://api.pinterest.com/v1/urls/count.json?url=" + (encodeURIComponent(url));
+    };
+
+    URLs.stumbleupon = function(url) {
+      var apiUrl, yql;
+      apiUrl = "http://www.stumbleupon.com/services/1.01/badge.getinfo?url=" + (encodeURIComponent(url));
+      yql = "SELECT * FROM json WHERE url='" + apiUrl + "'";
+      return "http://query.yahooapis.com/v1/public/yql?q=" + yql + "&format=json&diagnostics=true&callback=";
+    };
+
+    return URLs;
+
+  })();
+
+  BH.Counter = (function() {
+    function Counter(url, expires) {
+      this.url = url;
+      this.expires = expires != null ? expires : BH.expires;
+    }
+
+    Counter.prototype.cacheKey = function(key) {
+      return "Bookmarkhub.Counter.cacheKey(" + key + ")";
+    };
+
+    Counter.prototype.cachedRequest = function() {
+      var callback, me, options, rest, result, url;
+      url = arguments[0], rest = 2 <= arguments.length ? __slice.call(arguments, 1) : [];
+      callback = rest.pop();
+      options = rest.pop() || {};
+      result = BH.storage.load(this.cacheKey(url));
+      if (result) {
+        BH.trace("cached: " + url);
+        return callback(result);
+      }
+      me = this;
+      return this.request(url, options, function(data) {
+        BH.storage.save(me.cacheKey(url), data, me.expires);
+        return callback(data);
+      });
+    };
+
+    Counter.prototype.request = function() {
+      var callback, options, rest, url;
+      url = arguments[0], rest = 2 <= arguments.length ? __slice.call(arguments, 1) : [];
+      callback = rest.pop();
+      options = $.extend({
+        url: url,
+        type: 'get',
+        dataType: 'jsonp'
+      }, rest.pop() || {});
+      return $.ajax(options).done(function(data) {
+        return callback(data);
+      }).fail(function() {
+        var rest;
+        rest = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
+        return BH.trace('ajax fail: ', rest);
+      });
+    };
+
+    Counter.prototype.twitter = function(callback) {
+      return this.cachedRequest(BH.URLs.twitter(this.url), function(data) {
+        var result;
+        result = data.count || 0;
+        return callback(result);
+      });
+    };
+
+    Counter.prototype.hatena = function(callback) {
+      return this.cachedRequest(BH.URLs.hatena(this.url), function(data) {
+        return callback(data || 0);
+      });
+    };
+
+    Counter.prototype.facebook = function(callback) {
+      return this.cachedRequest(BH.URLs.facebook(this.url), function(data) {
+        return callback(data.shares || 0);
+      });
+    };
+
+    Counter.prototype.google = function(callback) {
+      return this.cachedRequest(BH.URLs.google(this.url), {
+        dataType: 'json'
+      }, function(data) {
+        return callback(data.query.results ? $(data.query.results.resources.content).find('#aggregateCount').text() : 0);
+      });
+    };
+
+    Counter.prototype.pocket = function(callback) {
+      var me, result;
+      result = BH.storage.load(this.cacheKey(this.url));
+      if (result) {
+        BH.trace("cached: " + this.url);
+        return callback(result);
+      }
+      me = this;
+      return this.request(BH.URLs.pocket(this.url), {
+        dataType: 'json'
+      }, function(data) {
+        result = data.query ? data.query.results.body.div.a.span.em.content : 0;
+        BH.storage.save(me.cacheKey(me.url), result, me.expires);
+        return callback(result);
+      });
+    };
+
+    Counter.prototype.linkedin = function(callback) {
+      return this.cachedRequest(BH.URLs.linkedin(this.url), function(data) {
+        return callback(data.count || 0);
+      });
+    };
+
+    Counter.prototype.delicious = function(callback) {
+      return this.cachedRequest(BH.URLs.delicious(this.url), function(data) {
+        var _ref;
+        return callback(((_ref = data[0]) != null ? _ref.total_posts : void 0) || 0);
+      });
+    };
+
+    Counter.prototype.pinterest = function(callback) {
+      return this.cachedRequest(BH.URLs.pinterest(this.url), function(data) {
+        return callback(data.count || 0);
+      });
+    };
+
+    Counter.prototype.stumbleupon = function(callback) {
+      return this.cachedRequest(BH.URLs.stumbleupon(this.url), {
+        dataType: 'json'
+      }, function(data) {
+        var _ref;
+        return callback(((_ref = data.query.results.json.result) != null ? _ref.views : void 0) || 0);
+      });
+    };
+
+    return Counter;
+
+  })();
+
+  BH.Linker = (function() {
+    function Linker(url, expires) {
+      this.url = url;
+      this.expires = expires != null ? expires : BH.expires;
+      this.counter = new BH.Counter(this.url, this.expires);
+    }
+
+    Linker.prototype.schemelessUrl = function() {
+      return encodeURIComponent(this.url.replace(/^https?:\/\//, ''));
+    };
+
+    Linker.prototype.twitter = function(callback) {
+      var me;
+      me = this;
+      return this.counter.twitter(function(count) {
+        return callback({
+          count: count,
+          link: count ? "https://twitter.com/search?q=" + (me.schemelessUrl()) : void 0
+        });
+      });
+    };
+
+    Linker.prototype.facebook = function(callback) {
+      return this.counter.facebook(function(count) {
+        return callback({
+          count: count
+        });
+      });
+    };
+
+    Linker.prototype.hatena = function(callback) {
+      var me;
+      me = this;
+      return this.counter.hatena(function(count) {
+        return callback({
+          count: count,
+          link: count ? "http://b.hatena.ne.jp/entry/" + (me.schemelessUrl()) : void 0
+        });
+      });
+    };
+
+    Linker.prototype.google = function(callback) {
+      return this.counter.google(function(count) {
+        return callback({
+          count: count
+        });
+      });
+    };
+
+    Linker.prototype.pocket = function(callback) {
+      return this.counter.pocket(function(count) {
+        return callback({
+          count: count
+        });
+      });
+    };
+
+    Linker.prototype.linkedin = function(callback) {
+      var me;
+      me = this;
+      return this.counter.linkedin(function(count) {
+        return callback({
+          count: count
+        });
+      });
+    };
+
+    Linker.prototype.delicious = function(callback) {
+      var me;
+      me = this;
+      return this.counter.delicious(function(count) {
+        return callback({
+          count: count,
+          link: count ? "https://previous.delicious.com/url/" + (BH.md5Hex(me.url)) : void 0
+        });
+      });
+    };
+
+    Linker.prototype.pinterest = function(callback) {
+      var me;
+      me = this;
+      return this.counter.pinterest(function(count) {
+        return callback({
+          count: count
+        });
+      });
+    };
+
+    Linker.prototype.stumbleupon = function(callback) {
+      var me;
+      me = this;
+      return this.counter.stumbleupon(function(count) {
+        return callback({
+          count: count,
+          link: count ? "http://www.stumbleupon.com/url/" + me.url : void 0
+        });
+      });
+    };
+
+    return Linker;
+
+  })();
+
+  BH.md5Hex = (function() {
+    var CC, MD5_F, MD5_G, MD5_H, MD5_I, MD5_T, MD5_apply_round, MD5_hash, MD5_hexhash, MD5_number, MD5_pack, MD5_round, MD5_round1, MD5_round2, MD5_round3, MD5_round4, MD5_unpack;
+    MD5_F = function(x, y, z) {
+      return (x & y) | (~x & z);
+    };
+    MD5_G = function(x, y, z) {
+      return (x & z) | (y & ~z);
+    };
+    MD5_H = function(x, y, z) {
+      return x ^ y ^ z;
+    };
+    MD5_I = function(x, y, z) {
+      return y ^ (x | ~z);
+    };
+    MD5_pack = function(n32) {
+      return CC(n32 & 0xff) + CC((n32 >>> 8) & 0xff) + CC((n32 >>> 16) & 0xff) + CC((n32 >>> 24) & 0xff);
+    };
+    MD5_unpack = function(s4) {
+      return s4.charCodeAt(0) | (s4.charCodeAt(1) << 8) | (s4.charCodeAt(2) << 16) | (s4.charCodeAt(3) << 24);
+    };
+    MD5_number = function(n) {
+      while (n < 0) {
+        n += 4294967296;
+      }
+      while (n > 4294967295) {
+        n -= 4294967296;
+      }
+      return n;
+    };
+    MD5_apply_round = function(x, s, f, abcd, r) {
+      var a, b, c, d, ii, kk, ss, t, u;
+      a = void 0;
+      b = void 0;
+      c = void 0;
+      d = void 0;
+      kk = void 0;
+      ss = void 0;
+      ii = void 0;
+      t = void 0;
+      u = void 0;
+      a = abcd[0];
+      b = abcd[1];
+      c = abcd[2];
+      d = abcd[3];
+      kk = r[0];
+      ss = r[1];
+      ii = r[2];
+      u = f(s[b], s[c], s[d]);
+      t = s[a] + u + x[kk] + MD5_T[ii];
+      t = MD5_number(t);
+      t = (t << ss) | (t >>> (32 - ss));
+      t += s[b];
+      return s[a] = MD5_number(t);
+    };
+    MD5_hash = function(data) {
+      var abcd, f, i, index, j, k, len, padLen, r, s, state, tmp, x;
+      abcd = void 0;
+      x = void 0;
+      state = void 0;
+      s = void 0;
+      len = void 0;
+      index = void 0;
+      padLen = void 0;
+      f = void 0;
+      r = void 0;
+      i = void 0;
+      j = void 0;
+      k = void 0;
+      tmp = void 0;
+      state = [0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476];
+      len = data.length;
+      index = len & 0x3f;
+      padLen = (index < 56 ? 56 - index : 120 - index);
+      if (padLen > 0) {
+        data += "";
+        i = 0;
+        while (i < padLen - 1) {
+          data += "\u0000";
+          i++;
+        }
+      }
+      data += MD5_pack(len * 8);
+      data += MD5_pack(0);
+      len += padLen + 8;
+      abcd = [0, 1, 2, 3];
+      x = new Array(16);
+      s = new Array(4);
+      k = 0;
+      while (k < len) {
+        i = 0;
+        j = k;
+        while (i < 16) {
+          x[i] = data.charCodeAt(j) | (data.charCodeAt(j + 1) << 8) | (data.charCodeAt(j + 2) << 16) | (data.charCodeAt(j + 3) << 24);
+          i++;
+          j += 4;
+        }
+        i = 0;
+        while (i < 4) {
+          s[i] = state[i];
+          i++;
+        }
+        i = 0;
+        while (i < 4) {
+          f = MD5_round[i][0];
+          r = MD5_round[i][1];
+          j = 0;
+          while (j < 16) {
+            MD5_apply_round(x, s, f, abcd, r[j]);
+            tmp = abcd[0];
+            abcd[0] = abcd[3];
+            abcd[3] = abcd[2];
+            abcd[2] = abcd[1];
+            abcd[1] = tmp;
+            j++;
+          }
+          i++;
+        }
+        i = 0;
+        while (i < 4) {
+          state[i] += s[i];
+          state[i] = MD5_number(state[i]);
+          i++;
+        }
+        k += 64;
+      }
+      return MD5_pack(state[0]) + MD5_pack(state[1]) + MD5_pack(state[2]) + MD5_pack(state[3]);
+    };
+    MD5_hexhash = function(data) {
+      var bit128, c, i, out;
+      i = void 0;
+      out = void 0;
+      c = void 0;
+      bit128 = MD5_hash(data);
+      out = "";
+      i = 0;
+      while (i < 16) {
+        c = bit128.charCodeAt(i);
+        out += "0123456789abcdef".charAt((c >> 4) & 0xf);
+        out += "0123456789abcdef".charAt(c & 0xf);
+        i++;
+      }
+      return out;
+    };
+    MD5_T = [0x00000000, 0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee, 0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501, 0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be, 0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821, 0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa, 0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8, 0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed, 0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a, 0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c, 0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70, 0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05, 0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665, 0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039, 0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1, 0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1, 0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391];
+    MD5_round1 = [[0, 7, 1], [1, 12, 2], [2, 17, 3], [3, 22, 4], [4, 7, 5], [5, 12, 6], [6, 17, 7], [7, 22, 8], [8, 7, 9], [9, 12, 10], [10, 17, 11], [11, 22, 12], [12, 7, 13], [13, 12, 14], [14, 17, 15], [15, 22, 16]];
+    MD5_round2 = [[1, 5, 17], [6, 9, 18], [11, 14, 19], [0, 20, 20], [5, 5, 21], [10, 9, 22], [15, 14, 23], [4, 20, 24], [9, 5, 25], [14, 9, 26], [3, 14, 27], [8, 20, 28], [13, 5, 29], [2, 9, 30], [7, 14, 31], [12, 20, 32]];
+    MD5_round3 = [[5, 4, 33], [8, 11, 34], [11, 16, 35], [14, 23, 36], [1, 4, 37], [4, 11, 38], [7, 16, 39], [10, 23, 40], [13, 4, 41], [0, 11, 42], [3, 16, 43], [6, 23, 44], [9, 4, 45], [12, 11, 46], [15, 16, 47], [2, 23, 48]];
+    MD5_round4 = [[0, 6, 49], [7, 10, 50], [14, 15, 51], [5, 21, 52], [12, 6, 53], [3, 10, 54], [10, 15, 55], [1, 21, 56], [8, 6, 57], [15, 10, 58], [6, 15, 59], [13, 21, 60], [4, 6, 61], [11, 10, 62], [2, 15, 63], [9, 21, 64]];
+    MD5_round = [[MD5_F, MD5_round1], [MD5_G, MD5_round2], [MD5_H, MD5_round3], [MD5_I, MD5_round4]];
+    CC = String.fromCharCode;
+    return MD5_hexhash;
+  })();
+
+}).call(this);
